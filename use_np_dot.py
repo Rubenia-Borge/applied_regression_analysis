d9c97bfd3767fe112d7b3d1f93a94e994a240413
Rubenia Borge <rubeniaolaska@gmail.com>
2022-08-27 12:10:16 -0500

    Update use_np_dot.py

--- use_np_dot.py
+++ use_np_dot.py
@@ -1,1 +1,17 @@
 
+# numpy library: scientific computing in Python, multidimensional array object, derived objects,
+# routines for fast operations in arrays
+# scipy library: algorithms for optimizatin, integration, interpolation, eigenvalue problems, 
+# algebraic equations, differential equations, statistics, etc.
+import numpy as np
+import scipy.stats as st
+
+x_dependent_variable = np.array([40, 100, 99, 150])
+y_independent_variable = np.array([1, 3, 2, 5])
+
+simple_linear_regression_model = st.linregress(x_dependent_variable,y_independent_variable)
+
+print(simple_linear_regression_model)
+
+
+np.dot([3, 1],[simple_linear_regression_model[0], simple_linear_regression_model[1]])
